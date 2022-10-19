47789e1b5b33167d9f368bd7c27e4f4484b35fa6
Bartholomé Gili <bartho.gili@gmail.com>
2022-10-19 10:14:42 +0200

    init: base structure + cli

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'public/styles/')],
+	}
 }
 
 module.exports = nextConfig
