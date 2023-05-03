d96deace13ce063d60b6f24a04837951ba227b3e
barthofu <dev.bartho@gmail.com>
2023-05-03 13:33:19 +0000

    feat(server): generic crud controllers

--- templates/next-js/main-stack/src/server/router/users.ts
+++ templates/next-js/main-stack/src/server/router/users.ts
@@ -1,50 +1,14 @@
-import { prisma } from '@server/prisma'
-import { publicProcedure, router } from '@server/trpc'
-import { TRPCError } from '@trpc/server'
+import { User } from '@prisma/client'
+import { router } from '@server/trpc'
+import { createController, getController, getAllController } from '@utils/functions'
 import { UserCreateOneSchema } from 'prisma/generated/schemas'
-import { z } from 'zod'
 
 export const usersRouter = router({
 
-    getAll: publicProcedure
-        .query(async () => {
+    // crud
+    getAll: getAllController<User>('user'),
+    get: getController<User>('user'),
+    create: createController<User>('user', UserCreateOneSchema),
 
-            const users = await prisma.user.findMany()
-            
-            return users
-        }),
-
-    get: publicProcedure
-        .input(z.object({
-            id: z.number()
-        }))
-        .query(async ({ input }) => {
-
-            const user = await prisma.user.findUnique({
-                where: {
-                    id: input.id
-                }
-            })
-
-            if (!user) {
-                throw new TRPCError({
-                    code: 'NOT_FOUND',
-                    message: 'User not found'
-                })
-            }
-
-            return user
-        }),
-
-    create: publicProcedure
-        .input(UserCreateOneSchema)
-        .query(async ({ input }) => {
-
-            const user = await prisma.user.create({
-                data: input.data
-            })
-
-            return user
-        }), 
-
+    // other
 })
